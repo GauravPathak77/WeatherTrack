cf8b2d5ea13ce711e4451cd4df12bc509539c383
GauravPathak77 <115170827+GauravPathak77@users.noreply.github.com>
2023-07-10 20:36:48 +0530

    WeatherTrack V1.0

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
 import requests
+from dotenv import dotenv_values
+import math
 
 app = Flask(__name__)
+
+config = dotenv_values(".env")
 
 
 @app.route('/')
@@ -12,12 +16,36 @@ def index():
 @app.route('/', methods=['POST'])
 def weather():
     city = request.form['city']
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid=c3a14b64fd3eb5f994230183700f79d1&units=metric'
-    response = requests.get(url)
+    api_key = config['API']
+    weather_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+
+    response = requests.get(weather_url)
     data = response.json()
-    temperature = data['main']['temp']
-    weather_desc = data['weather'][0]['description']
-    return render_template('index.html', temperature=temperature, weather_desc=weather_desc)
+
+    if data['cod'] != '404':
+        lat = data['coord']['lat']
+        lon = data['coord']['lon']
+        z = 10
+        n = 2 ** z
+        x = math.floor((lon + 180) / 360 * n)
+        y = math.floor(
+            (1 - math.log(math.tan(lat * math.pi / 180) + 1 / math.cos(lat * math.pi / 180)) / math.pi) / 2 * n)
+        # map_url = f'https://www.google.com/maps/place/{city}/@{lat},{lon},10z'
+        icon = data['weather'][0]['icon']
+        ImageURL =  f'http://openweathermap.org/img/wn/"{icon}@2x.png'
+        weather_info = {
+            'city': data['name'],
+            'temperature': data['main']['temp'],
+            'humidity': data['main']['humidity'],
+            'description': data['weather'][0]['description'],
+            'icon': data['weather'][0]['icon'],
+            'map_url': map_url,
+            'image' : ImageURL,
+        }
+    else:
+        weather_info = None
+
+    return render_template('index.html', weather_info=weather_info)
 
 
 if __name__ == '__main__':
